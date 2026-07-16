ddc519b175ad669c373834fa4b8de3ecddbd5dff
Maria Violante <ViolanteM@vanburencountymi.gov>
2026-07-16 12:56:55 -0400

    add is_authenticated_user check

--- permissions/checks.py
+++ permissions/checks.py
@@ -2,12 +2,18 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from permissions.services import AdministratorGroupService
+from permissions.admin_group_service import AdministratorGroupService
 
 if TYPE_CHECKING:
     from django.contrib.auth.models import AnonymousUser
 
     from accounts.models import User
+
+
+def is_authenticated_user(user: User | AnonymousUser | None) -> bool:
+    if not user:
+        return False
+    return user.is_authenticated
 
 
 def is_a_department_owner(user: User | AnonymousUser | None) -> bool:
