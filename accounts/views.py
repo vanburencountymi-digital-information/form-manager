74a3c698c7c1133b98af6d0fd3112e902892c975
Maria Violante <ViolanteM@vanburencountymi.gov>
2026-07-17 11:19:35 -0400

    remove unused decorator and update invite_user view

--- accounts/views.py
+++ accounts/views.py
@@ -3,9 +3,11 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import redirect, render
 
-from permissions.decorators import admin_or_dept_owner_required
+from permissions.checks import can_manage_department_users
+from permissions.guards import assert_authenticated_user
 from permissions.services.admin_group_service import AdministratorGroupService
 
 from .forms import InviteUserForm
@@ -15,9 +17,10 @@ if TYPE_CHECKING:
 
 
 @login_required
-@admin_or_dept_owner_required
 def invite_user(request: HttpRequest) -> HttpResponse:
-    user = request.user
+    user = assert_authenticated_user(request.user)
+    if not can_manage_department_users(user):
+        raise PermissionDenied
 
     if request.method == "POST":
         form = InviteUserForm(request.POST, user=user)
